f0e4f474ef5636ac387b790a39c39ece0556f038
Surya Jeet Singh <suryajeetiitkgp@gmail.com>
2017-08-28 16:37:17 +0530

    First Coomit working model

---
+++ app/src/main/java/com/surya945/cc/enviromentmonitor/DataAndFilteredData.java
@@ -0,0 +1,16 @@
+package com.surya945.cc.enviromentmonitor;
+
+/**
+ * Created by cc on 13-08-2017.
+ */
+
+public class DataAndFilteredData {
+    int index;
+    double data;
+    double filteredData;
+    public DataAndFilteredData(int index, double data, double filteredData){
+        this.index=index;
+        this.data=data;
+        this.filteredData=filteredData;
+    }
+}
---
+++ app/src/main/java/com/surya945/cc/enviromentmonitor/Utility.java
@@ -0,0 +1,17 @@
+package com.surya945.cc.enviromentmonitor;
+
+/**
+ * Created by cc on 13-08-2017.
+ */
+
+public class Utility {
+    private Utility(){}
+
+    public static final int Accelerometer =9;
+    public static final int GyroSensor =10;
+    public static final int Light =11;
+    public static final int Mag =12;
+
+
+    public static int GraphSize=200;
+}
